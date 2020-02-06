265af4a1f80afcfb635a321b0ed2f6d29cfe6e9d
fanxing <2895166142@qq.com>
2020-02-06 22:20:01 +0800

    2020-02-06_05

--- js/login/login.js
+++ js/login/login.js
@@ -3,13 +3,13 @@ function checkForm() {
 	var psd = document.getElementById('logpass');
 	var a = "淮思蕊";
 	var b = "520";
+	var a1 = "淮思蕊520";
+	var b2 = "1314";
 	if((username.value == a) && (psd.value == b)) {
 		alert("欢迎访问gcwlovehsr的情侣空间！");
 		self.location.href = "https://繁星2018.top/nr/";
 		return true;
 	}
-	var a1 = "淮思蕊520";
-	var b2 = "1314";
 	if((username.value == a1) && (psd.value == b2)) {
 		alert("欢迎访问love-H的情侣空间！");
 		self.location.href = "https://www.gcwlovehsr.top/html/one/lovehsr.html";
